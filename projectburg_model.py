2f49e1fb583ba327ba5c054ef973b3575afd2cd7
ptrvandenberg <ptr.vandenberg@gmail.com>
2016-04-15 21:19:26 +1000

    Update projectburg_model.py

--- projectburg_model.py
+++ projectburg_model.py
@@ -13,7 +13,7 @@ dataFactory = TicDatFactory (
     days = [["dayseq"], []],
     shifts = [["shiftid"], ["type", "shift", "starttime", "crew", "description"]],
     members = [["memid"], ["ranknum", "lastname", "firstname"]],
-    carryover = [["memid"], ["lastWEoff", "day0shift"]]),
+    carryover = [["memid"], ["lastWEoff", "day0shift"]],
     leave = [["memid", "dayseq"], ["value"]],
     commitment = [["memid", "dayseq"], ["value"]],
     fieldout = [["memid", "dayseq"], ["value"]])
@@ -228,9 +228,11 @@ def solve(dat, week_res, shiftweek_res, shift_res):
 
     for m in dat.members:
         for d in dat.days:
-            m.addConstr(x_ds[m,d] = (if dat.member[m]["ranknum"] <> 5 then 0 else x_ds[m,d]), "Sgt_Rank")
-            m.addConstr(x_sg[m,d] = (if dat.member[m]["ranknum"] == 5 then 1 - x_ds[m,d] - x_or[m,d] - x_x[m,d] else x_sg[m,d]), "Sgt_Stat")
-            m.addConstr(x_sg[m,d] = (if dat.member[m]["ranknum"] == 6 then 1 - x_x[m,d] else x_sg[m,d]), "SenSgr_Stat")
+        for d in dat.days:
+            if dat.member[m]["ranknum"] != 5:
+                m.addConstr(x_ds[m,d] = 0, "Sgt_Rank")
+            m.addConstr(x_sg[m,d] = 1 - x_ds[m,d] - x_or[m,d] - x_x[m,d] if dat.member[m]["ranknum"] == 5, "Sgt_Stat")
+            m.addConstr(x_sg[m,d] = 1 - x_x[m,d] if dat.member[m]["ranknum"] == 6, "SenSgr_Stat")
 
     # AMPL: s.t. Sgt_WE {we in 0..1, w in WEEK, m in MEMBER}: x_x[1+6*we+7*(w-1),m] = (if memrank[m] = 5 then 1 - x_ds[1+6*we+7*(w-1),m] - x_or[1+6*we+7*(w-1),m] else x_x[1+6*we+7*(w-1),m]);
     # AMPL: s.t. SenSgt_WE {we in 0..1, w in WEEK, m in MEMBER}: x_x[1+6*we+7*(w-1),m] = (if memrank[m] = 6 then 1 else x_x[1+6*we+7*(w-1),m]);
